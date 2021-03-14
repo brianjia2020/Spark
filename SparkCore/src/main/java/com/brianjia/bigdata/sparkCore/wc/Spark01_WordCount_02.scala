2536496a9e8423d069bbbaef3f1510d1342281bf
chunyangjia <brianjia1123@gmail.com>
2021-03-14 17:43:50 -0400

    new commit

--- SparkCore/src/main/java/com/brianjia/bigdata/sparkCore/wc/Spark01_WordCount_02.scala
+++ SparkCore/src/main/java/com/brianjia/bigdata/sparkCore/wc/Spark01_WordCount_02.scala
@@ -3,7 +3,7 @@ package com.brianjia.bigdata.sparkCore.wc
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Spark01_WordCount {
+object Spark01_WordCount_02 {
   def main(args: Array[String]): Unit = {
     //Application
     //Spark framework
@@ -20,13 +20,24 @@ object Spark01_WordCount {
     //2. break each line into individual words
     val words: RDD[String] = lines.flatMap(_.split(" "))
 
+    val wordToOne = words.map(
+      word => (word, 1)
+    )
+
     //3. count the number of words
-    val wordGroup:RDD[(String, Iterable[String])] = words.groupBy(word => word)
-    val wordToCount: RDD[(String, Int)] = wordGroup.map{
-      case(word, list) => {
-        (word,list.size)
-      }
-    }
+//    val wordGroup: RDD[(String, Iterable[(String, Int)])] = wordToOne.groupBy(_._1)
+
+//    val wordToCount: RDD[(String, Int)] = wordGroup.map{
+//      case(word, list) => {
+//        list.reduce(
+//          (t1, t2) => {
+//            (t1._1, t1._2 + t2._2)
+//          }
+//        )
+//      }
+//    }
+
+    val wordToCount = wordToOne.reduceByKey((x,y)=>{x+y})
 
     //4. print to console
     val tuples: Array[(String, Int)] = wordToCount.collect()
