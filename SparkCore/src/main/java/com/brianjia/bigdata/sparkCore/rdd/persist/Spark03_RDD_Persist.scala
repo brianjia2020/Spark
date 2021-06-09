8f32e7cfa4e0ea0976ebedb2a8cab9971a32140c
brianjia2020 <brianjia1123@gmail.com>
2021-06-08 21:41:24 -0400

    new commit

--- SparkCore/src/main/java/com/brianjia/bigdata/sparkCore/rdd/persist/Spark03_RDD_Persist.scala
+++ SparkCore/src/main/java/com/brianjia/bigdata/sparkCore/rdd/persist/Spark03_RDD_Persist.scala
@@ -1,20 +1,25 @@
 package com.brianjia.bigdata.sparkCore.rdd.persist
 
-import org.apache.hadoop.hdfs.server.common.Storage
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Spark02_RDD_Persist {
+object Spark03_RDD_Persist {
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(sparkConf)
+    sc.setCheckpointDir("cp")
 
     val list = List("Hello Scala", "Hello Spark")
     val rdd = sc.makeRDD(list)
     val flatRdd = rdd.flatMap(_.split(" "))
-    val mapRDD = flatRdd.map((_,1))
-//    mapRDD.cache()
-    mapRDD.persist(StorageLevel.DISK_ONLY)
+    val mapRDD = flatRdd.map(word => {
+      println("@@@@@@@@@@")
+      (word,1)
+    })
+    //checkpoint is to fall onto the disk and has set save path
+    //checkpoint is after the check, it won't be saved
+    //usually
+    mapRDD.checkpoint()
     val reduceRDD = mapRDD.reduceByKey(_+_)
     reduceRDD.collect().foreach(println)
     println("******************")
--- SparkCore/src/main/java/com/brianjia/bigdata/sparkCore/rdd/persist/Spark04_RDD_Persist.scala
+++ SparkCore/src/main/java/com/brianjia/bigdata/sparkCore/rdd/persist/Spark04_RDD_Persist.scala
@@ -1,20 +1,30 @@
 package com.brianjia.bigdata.sparkCore.rdd.persist
 
-import org.apache.hadoop.hdfs.server.common.Storage
-import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Spark02_RDD_Persist {
+object Spark04_RDD_Persist {
   def main(args: Array[String]): Unit = {
+    //cache: temporarily store data in the memory, not very safe
+    //persist: temporarily store data in the disk or mem as specified
+    //         may involve disk IO, slow but safe
+    //         after the job has been done, the data will be deleted
+    //checkpoint: permanently store the data in the disk
+    //            to ensure data security, will perform the task separately
     val sparkConf = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(sparkConf)
+    sc.setCheckpointDir("cp")
 
     val list = List("Hello Scala", "Hello Spark")
     val rdd = sc.makeRDD(list)
     val flatRdd = rdd.flatMap(_.split(" "))
-    val mapRDD = flatRdd.map((_,1))
-//    mapRDD.cache()
-    mapRDD.persist(StorageLevel.DISK_ONLY)
+    val mapRDD = flatRdd.map(word => {
+      println("@@@@@@@@@@")
+      (word,1)
+    })
+    //checkpoint is to fall onto the disk and has set save path
+    //checkpoint is after the check, it won't be saved
+    //usually
+    mapRDD.checkpoint()
     val reduceRDD = mapRDD.reduceByKey(_+_)
     reduceRDD.collect().foreach(println)
     println("******************")
